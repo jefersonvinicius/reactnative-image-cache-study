c8981390889d7366d7de128227ac0e2631195c84
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2024-02-19 20:54:13 -0300

    Add stale option

--- App.tsx
+++ App.tsx
@@ -3,10 +3,12 @@ import {
   Image,
   ImageProps,
   ImageSourcePropType,
+  ScrollView,
   StyleSheet,
   View,
 } from 'react-native';
 import * as FileSystem from 'react-native-fs';
+import {humanFileSize} from './utils';
 
 console.log('DocumentDirectoryPath: ', FileSystem.DocumentDirectoryPath);
 console.log('CachesDirectoryPath: ', FileSystem.CachesDirectoryPath);
@@ -18,21 +20,38 @@ type Props = ImageProps & {
   onCacheLoad?: () => void;
 };
 
-function ImageCache({url, cacheKey, ...props}: Props) {
+function ImageCache({url, cacheKey, stale, ...props}: Props) {
   const isCachedImage = !!url && !!cacheKey;
-  console.log({isCachedImage});
-  const [source, setSource] = useState<ImageSourcePropType>({uri: undefined});
+  const [source, setSource] = useState<ImageSourcePropType | undefined>(
+    undefined,
+  );
 
   useEffect(() => {
     if (!url) return;
 
     (async function () {
       const filePath = `${FileSystem.CachesDirectoryPath}/${cacheKey}.jpg`;
+
+      const downloadFile = async () => {
+        const downloadResult = await FileSystem.downloadFile({
+          fromUrl: url,
+          toFile: filePath,
+        }).promise;
+        console.log('FILE SAVED INTO FILESYTEM: ', filePath);
+        console.log(`FILE SIZE: ${humanFileSize(downloadResult.bytesWritten)}`);
+      };
+
       const readFile = async () => {
         try {
-          const fileBase64 = await FileSystem.readFile(filePath, 'base64');
+          let fileBase64 = await FileSystem.readFile(filePath, 'base64');
           const stat = await FileSystem.stat(filePath);
           const time = Date.now() - new Date(stat.ctime).getTime();
+          if (stale && time > stale) {
+            console.log('STALE IMAGE, DOWNLOADING AGAIN...');
+            await FileSystem.unlink(filePath);
+            await downloadFile();
+            fileBase64 = await FileSystem.readFile(filePath, 'base64');
+          }
           setSource({uri: `data:image/jpg;base64,${fileBase64}`});
         } catch (error) {
           throw error;
@@ -43,11 +62,7 @@ function ImageCache({url, cacheKey, ...props}: Props) {
         console.log(error.code);
         if (error.code === 'ENOENT') {
           try {
-            await FileSystem.downloadFile({
-              fromUrl: url,
-              toFile: filePath,
-            }).promise;
-            console.log('FILE SAVED INTO FILESYTEM: ', filePath);
+            await downloadFile();
             readFile();
           } catch (downloadError) {
             console.log('ERROR ON DOWNLOAD');
@@ -55,7 +70,7 @@ function ImageCache({url, cacheKey, ...props}: Props) {
         }
       });
     })();
-  }, [cacheKey, url]);
+  }, [cacheKey, stale, url]);
 
   return (
     <Image
@@ -68,6 +83,9 @@ function ImageCache({url, cacheKey, ...props}: Props) {
 const IMAGE_JULIUS =
   'https://noticiasdatv.uol.com.br/media/uploads/everybody-hates-chris-julius-dia-dos-pais.jpg';
 
+const LANDSCAPE_IMAGE =
+  'https://upload.wikimedia.org/wikipedia/commons/3/3f/Fronalpstock_big.jpg';
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -75,8 +93,18 @@ export default function App() {
         url={IMAGE_JULIUS}
         style={styles.image}
         cacheKey="julius"
-        stale={5000}
+        // stale={5000}
       />
+      <ScrollView>
+        {Array.from({length: 10}).map((_, index) => (
+          <ImageCache
+            url={LANDSCAPE_IMAGE}
+            style={styles.image}
+            cacheKey={`landscape-${index}`}
+            // stale={5000}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
